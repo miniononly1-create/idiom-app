5616f427e57709cd357629253cbae5ca539d98ce
miniononly1-create <minion.only1@gmail.com>
2026-09-18 07:58:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jukugo-ai-pwa-v5';
+const CACHE_NAME = 'jukugo-ai-pwa-v6';
 const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
